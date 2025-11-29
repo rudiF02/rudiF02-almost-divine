93b6687c602ac26f406892329f11b50df991e36d
TakeOff30 <oliver20669@gmail.com>
2025-11-29 16:53:19 +0100

    I don't fear you, merge conflict

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var is_dead: bool = false
+const BLOCK_STATE_FALLING = 0
+const BLOCK_STATE_LOCKED = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +15,19 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		print(collider)
+		if "current_state" in collider:
+			
+			if collider.current_state == BLOCK_STATE_FALLING:
+				print("Touched a falling block - DIE")
+				die()
+			
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +38,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func die():
+	if is_dead: return
+	is_dead = true
+	print("Game Over")

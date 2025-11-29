00b7469037f1a639dcb494d02f976c6c5d435fa9
samu21-code <samuelemoretti2001@gmail.com>
2025-11-29 23:08:10 +0100

    added restart logic

--- godot/scenes/game_over.gd
+++ godot/scenes/game_over.gd
@@ -4,3 +4,7 @@ extends Control
 func _on_quit_button_pressed():
 	# Quits the game
 	get_tree().quit()
+
+func _on_restart_button_pressed():
+	# Reloads the main game scene
+	get_tree().change_scene_to_file("res://scenes/main_menu_scene.tscn")
